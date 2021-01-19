5c0e818ad0cf7fb61be1cb8cbb594eb7fa962413
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 16:53:42 +0100

    yabause: initial package

---
+++ packages/supervisedthinking/emulation/libretro/yabause/package.mk
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="yabause"
+PKG_VERSION="1e981f18e763c0897e4dd2ec79fab927a483e626"
+PKG_SHA256="8ac011d06908ced1536f792246fdc6218fddfdfec07da7d8a5d78c9b6949eab9"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/yabause"
+PKG_URL="https://github.com/libretro/yabause/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_LONGDESC="Yabause is a Sega Saturn emulator."
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
+
+PKG_LIBNAME="yabause_libretro.so"
+PKG_LIBPATH="yabause/src/libretro/${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="-C yabause/src/libretro GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_configure_target() {
+  if [ "${ARCH}" = "arm" ]; then
+    PKG_MAKE_OPTS_TARGET+=" platform=armv"
+    # ARM NEON support
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+="-neon"
+    fi
+    PKG_MAKE_OPTS_TARGET+="-${TARGET_FLOAT}float-${TARGET_CPU}"
+  fi
+}
+
+pre_make_target() {
+  make CC=${HOST_CC} -C yabause/src/libretro generate-files
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

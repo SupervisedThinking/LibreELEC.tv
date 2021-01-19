61d6c10812f087e88f8d787e5f3fadaf4070a976
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 16:46:27 +0100

    fbneo: initial package

---
+++ packages/supervisedthinking/emulation/libretro/fbneo/package.mk
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="fbneo"
+PKG_VERSION="67f87d5e58ac4bbc88c61d263ecb2af7faea49f3"
+PKG_SHA256="cc3b588d18dcf207e0e6bba258e9dfc602a3e1c7602f7db752ec34d5d36414ed"
+PKG_LICENSE="OSS"
+PKG_SITE="https://github.com/libretro/FBNeo"
+PKG_URL="https://github.com/libretro/FBNeo/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_LONGDESC="A libretro port of FinalBurn Neo for Romset v0.2.97.44"
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
+
+PKG_LIBNAME="fbneo_libretro.so"
+PKG_LIBPATH="src/burner/libretro/${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="-C src/burner/libretro/ GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_configure_target() {
+  if [ "${PROJECT}" = "RPi" ]; then
+    case ${DEVICE} in
+      RPi)
+        PKG_MAKE_OPTS_TARGET+=" platform=armv"
+        ;;
+      RPi2)
+        PKG_MAKE_OPTS_TARGET+=" platform=rpi2"
+        ;;
+    esac
+  else
+    # NEON Support ?
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+=" HAVE_NEON=1"
+    fi
+  fi
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

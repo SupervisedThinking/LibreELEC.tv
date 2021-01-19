095e1e1214b1ac8d4e7531dcbaa7f09a7002aba7
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 16:53:09 +0100

    uae: initial package

---
+++ packages/supervisedthinking/emulation/libretro/uae/package.mk
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="uae"
+PKG_VERSION="2b573d39eedc73b8b43db8dbe71d7bc17a32adac"
+PKG_SHA256="d08ca21917ba6e0fa3ea46f38f6910b0ff37fb23919e80cd3b3440fafb0fe59e"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/libretro/libretro-uae"
+PKG_URL="https://github.com/libretro/libretro-uae/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_LONGDESC="Libretro wrapper for UAE emulator."
+PKG_TOOLCHAIN="make"
+
+PKG_LIBNAME="puae_libretro.so"
+PKG_LIBPATH="${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_configure_target() {
+  if [ "${ARCH}" = "arm" ]; then
+    if [ "${PROJECT}" = "RPi" ]; then
+      PKG_MAKE_OPTS_TARGET+=" platform=rpi"
+    else
+      PKG_MAKE_OPTS_TARGET+=" platform=armv"
+      # ARM NEON support
+      if target_has_feature neon; then
+        PKG_MAKE_OPTS_TARGET+="-neon"
+      fi
+      PKG_MAKE_OPTS_TARGET+="-${TARGET_FLOAT}float-${TARGET_CPU}"
+    fi
+  fi
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

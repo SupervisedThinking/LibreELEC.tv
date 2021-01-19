8276522a524f22d969d99e14d12e7b71c8724a32
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 16:41:22 +0100

    beetle-wswan: initial package

---
+++ packages/supervisedthinking/emulation/libretro/beetle-wswan/package.mk
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="beetle-wswan"
+PKG_VERSION="362b5c05ca4e063b1a53f2eb132dc126bfe1ea82"
+PKG_SHA256="ed5fdfe8e85260a47e0cf026a1675701805ed3546afe876395067953a47ff86c"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/beetle-wswan-libretro"
+PKG_URL="https://github.com/libretro/beetle-wswan-libretro/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_LONGDESC="Standalone port of Mednafen WonderSwan to libretro, itself a fork of Cygne."
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
+
+PKG_LIBNAME="mednafen_wswan_libretro.so"
+PKG_LIBPATH="${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

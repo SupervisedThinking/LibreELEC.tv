276a3a5534d2bfe0702562329079ba2020a42685
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 16:46:19 +0100

    dosbox-pure: initial package

---
+++ packages/supervisedthinking/emulation/libretro/dosbox-pure/package.mk
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="dosbox-pure"
+PKG_VERSION="33e6bf25e83320a32e43336f13f896c4edae6b30"
+PKG_SHA256="e8deffcf88175070f115511481bc8380e00d123971fac032c0f3493bc0349314"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/dosbox-pure"
+PKG_URL="https://github.com/libretro/dosbox-pure/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_LONGDESC="DOSBox Pure is a fork of DOSBox, an emulator for DOS games, built for RetroArch/Libretro aiming for simplicity and ease of use."
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS+="+lto"
+
+PKG_LIBNAME="dosbox_pure_libretro.so"
+PKG_LIBPATH="${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

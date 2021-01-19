af76ed629fe38af9902fe0cfdb432fae95f9dddb
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 16:53:03 +0100

    tyrquake: initial package

---
+++ packages/supervisedthinking/emulation/libretro/tyrquake/package.mk
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="tyrquake"
+PKG_VERSION="371305cde12ca97ee2161f968121d406a0f65406"
+PKG_SHA256="0eb52518b125b453dbb651553b1a8f0d1606edef5f9fa9e27365aacea3a427ce"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/libretro/tyrquake"
+PKG_URL="https://github.com/libretro/tyrquake/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_LONGDESC="Libretro port of Tyrquake (Quake 1 engine)"
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
+
+PKG_LIBNAME="tyrquake_libretro.so"
+PKG_LIBPATH="${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/bin
+  mkdir -p ${INSTALL}/usr/lib/libretro
+
+  cp ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

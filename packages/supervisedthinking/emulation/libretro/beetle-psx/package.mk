af72b6ad11e66a8a36c3ba89a6b34ae7f4c7467c
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 16:40:59 +0100

    beetle-psx: initial package

---
+++ packages/supervisedthinking/emulation/libretro/beetle-psx/package.mk
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="beetle-psx"
+PKG_VERSION="e1836bde1b250d41c6f91c2b6f06444d989f1138"
+PKG_SHA256="1495fc8f7fcaeb2b1f1ae9d8b5a0ffaad0fc0cd72634746964b42b67bc3a9732"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/beetle-psx-libretro"
+PKG_URL="https://github.com/libretro/beetle-psx-libretro/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_LONGDESC="Standalone port/fork of Mednafen PSX to the Libretro API."
+PKG_BUILD_FLAGS="+lto"
+
+PKG_LIBNAME="mednafen_psx_*libretro.so"
+PKG_LIBPATH="${PKG_LIBNAME}"
+
+configure_package() {
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" xorg-server"
+  fi
+
+  # OpenGL Support
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  fi
+}
+
+make_target() {
+  make GIT_VERSION=${PKG_VERSION:0:7}
+
+  # Build with OpenGL/Vulkan support if available
+  if [ "${OPENGL_SUPPORT}" = "yes" ];then
+    mkdir -p tmp
+    mv ${PKG_LIBNAME} tmp/
+    make clean
+    if [ "${VULKAN_SUPPORT}" = "yes" ];then
+      make HAVE_HW=1 GIT_VERSION=${PKG_VERSION:0:7}
+    else
+      make HAVE_OPENGL=1 GIT_VERSION=${PKG_VERSION:0:7}
+    fi
+    mv tmp/${PKG_LIBNAME} .
+  fi
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

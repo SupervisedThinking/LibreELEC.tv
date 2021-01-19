00984f882de5139183034f12a8db9b5feb520289
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 17:35:48 +0100

    slang-shaders-lr: initial package

---
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/slang-shaders-lr/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="slang-shaders-lr"
+PKG_VERSION="65a89c93d09efb68ed50c2770c99fe14c56b6409"
+PKG_SHA256="eb4d55aeb30a2a262f4a6f03fab6a9ab70a7646ae22d152f8c0c955ae5107074"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/libretro/slang-shaders"
+PKG_URL="https://github.com/libretro/slang-shaders/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="gcc:host"
+PKG_LONGDESC="Vulkan GLSL RetroArch shader system"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  cd ${PKG_BUILD}
+  make install INSTALLDIR="$INSTALL/usr/share/retroarch/shaders/Vulkan-Shaders"
+}

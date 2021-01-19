d0d97106cc1f7f1caa5fdd02aa699159184e073b
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 16:53:20 +0100

    vice-libretro: initial package

---
+++ packages/supervisedthinking/emulation/libretro/vice-libretro/package.mk
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="vice-libretro"
+PKG_VERSION="69ebb7942b837bf07dd13a04d98621f64cb945c0"
+PKG_SHA256="5ea62ec071493aca355bbd50ac76c4def80aeab1ce12c9036441bd5e17fada51"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/vice-libretro"
+PKG_URL="https://github.com/libretro/vice-libretro/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_LONGDESC="Versatile Commodore 8-bit Emulator version 3.3 LIBRETRO WIP"
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
+
+PKG_LIBNAME="vice_x64_libretro.so"
+PKG_LIBPATH="${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_configure_target() {
+  if [ "${ARCH}" = "arm" ]; then
+    PKG_MAKE_OPTS_TARGET+=" platform=armv"
+
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+="-neon"
+    fi
+  fi
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

dc6443c2b50abd5f5c53c808b1ea932775c52b00
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 17:08:08 +0100

    vulkan-loader: initial package

---
+++ packages/supervisedthinking/graphics/vulkan/vulkan-loader/package.mk
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="vulkan-loader"
+PKG_VERSION="1.2.166"
+PKG_SHA256="1094b303ead1843fd31a5c11b0f5c2f91949b0608f36619bf92c738f6d561b35"
+PKG_LICENSE="Apache 2.0"
+PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain cmake:host vulkan-headers"
+PKG_LONGDESC="Vulkan Installable Client Driver (ICD) Loader."
+
+configure_package() {
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" libX11 libXrandr libxcb"
+  fi
+}
+
+pre_configure_target() {
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_TESTS=Off \
+                         -DBUILD_WSI_WAYLAND_SUPPORT=Off"
+
+  # Mali Vulkan support
+  if [ ! "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_XCB_SUPPORT=Off \
+                             -DBUILD_WSI_XLIB_SUPPORT=Off"
+  fi
+}

a64fd4084af5154e2b0ea16d4996dfa0169227f4
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 16:52:44 +0100

    snes9x2010: initial package

---
+++ packages/supervisedthinking/emulation/libretro/snes9x2010/package.mk
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="snes9x2010"
+PKG_VERSION="bea042df986703233b3e7229d23dc17f2cc90258"
+PKG_SHA256="36f42874ba0043ed0ed165df0c4c5913868bbf5908999522514c458c31a162ed"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/libretro/snes9x2010"
+PKG_URL="https://github.com/libretro/snes9x2010/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_LONGDESC="Snes9x 2010. Port of Snes9x 1.52+ to Libretro (previously called SNES9x Next). Rewritten in C and several optimizations and speedhacks."
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
+
+PKG_LIBNAME="snes9x2010_libretro.so"
+PKG_LIBPATH="${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

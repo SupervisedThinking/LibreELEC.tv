758f975e96da074e9adb8b4a7aa2e2095bcfc506
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 17:34:39 +0100

    database-lr: initial package

---
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/database-lr/package.mk
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="database-lr"
+PKG_VERSION="1.9.0"
+PKG_SHA256="4b04c973aff056a101086bd46439c75e524f4f697dd09011118435871d340ddd"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/libretro/libretro-database"
+PKG_URL="https://github.com/libretro/libretro-database/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="gcc:host"
+PKG_LONGDESC="RetroArch database containing cheatcode files, content data files, etc."
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  cd ${PKG_BUILD}
+  make install INSTALLDIR="${INSTALL}/usr/share/retroarch/database"
+}
+
+post_makeinstall_target() {
+  # Remove common unnecessary databases
+  safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/MAME.rdb
+  safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/MAME*2000.rdb
+  safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/MAME*2003.rdb
+  safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/MAME*2015.rdb
+  safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/Microsoft*Xbox*.rdb
+  safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/Sony*PlayStation*3*.rdb
+
+  # Remove additional unnecessary databases
+  if [ ! "${PROJECT}" = "Generic" ]; then
+    safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/MAME*2016.rdb
+    safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/Nintendo*GameCube*.rdb
+    safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/Nintendo*Nintendo*3DS*.rdb
+    safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/Nintendo*Wii*.rdb
+  elif [ "${PROJECT}" = "Generic" ]; then
+    safe_remove ${INSTALL}/usr/share/retroarch/database/rdb/MAME*2010.rdb
+  fi
+}

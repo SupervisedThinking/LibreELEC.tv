b584a65ca609b2c040befc0b67adba45f73e449d
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 16:41:11 +0100

    beetle-saturn: initial package

---
+++ packages/supervisedthinking/emulation/libretro/beetle-saturn/package.mk
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="beetle-saturn"
+PKG_VERSION="6781645f170425473d03f896e6a5985dbe7b9d4b"
+PKG_SHA256="05c35997e1cea9f6fa59cbc5c028302cc7c7fae40e05a3a5363dda5dd91e3a5f"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/libretro/beetle-saturn-libretro"
+PKG_URL="https://github.com/libretro/beetle-saturn-libretro/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_LONGDESC="Standalone port of Mednafen Saturn to the libretro API."
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
+
+PKG_LIBNAME="mednafen_saturn_libretro.so"
+PKG_LIBPATH="${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

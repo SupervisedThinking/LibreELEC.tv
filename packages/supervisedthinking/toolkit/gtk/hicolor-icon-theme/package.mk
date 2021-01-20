41a67be73bdd197084679441f6e862435ab1edf8
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-20 16:11:12 +0100

    hicolor-icon-theme: fix dependings

--- packages/supervisedthinking/toolkit/gtk/hicolor-icon-theme/package.mk
+++ packages/supervisedthinking/toolkit/gtk/hicolor-icon-theme/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="317484352271d18cbbcfac3868eab798d67fff1b8402e740baa6ff41d588a9d8"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.freedesktop.org/wiki/Software/icon-theme/"
 PKG_URL="https://icon-theme.freedesktop.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain gtk3"
+PKG_DEPENDS_TARGET="toolchain gtk3-system"
 PKG_LONGDESC="icon-theme contains the standard also references the default icon theme called hicolor."
 PKG_BUILD_FLAGS="-sysroot"
 

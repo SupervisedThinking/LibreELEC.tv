cafd3462fcf4c67ac8d0cf04c0028a0ea8db7e0f
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-21 20:05:02 +0100

    dmidecode: automated code cleanup

--- packages/supervisedthinking/sysutils/dmidecode/package.mk
+++ packages/supervisedthinking/sysutils/dmidecode/package.mk
@@ -7,10 +7,10 @@ PKG_SHA256="82c737a780614c38a783e8055340d295e332fb12c7f418b5d21a0797d3fb1455"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.nongnu.org/dmidecode/"
-PKG_URL="http://download.savannah.gnu.org/releases/dmidecode/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="http://download.savannah.gnu.org/releases/dmidecode/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="dmidecode: report DMI information"
 
 makeinstall_target() {
-  make prefix=$INSTALL/usr install-bin
+  make prefix=${INSTALL}/usr install-bin
 }

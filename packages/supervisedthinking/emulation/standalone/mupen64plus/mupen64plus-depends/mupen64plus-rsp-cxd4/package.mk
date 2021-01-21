ae17226aca027583e535935d783e59bda55a78a8
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-21 20:07:09 +0100

    mupen64plus-rsp-cxd4: automated code cleanup

--- packages/supervisedthinking/emulation/standalone/mupen64plus/mupen64plus-depends/mupen64plus-rsp-cxd4/package.mk
+++ packages/supervisedthinking/emulation/standalone/mupen64plus/mupen64plus-depends/mupen64plus-rsp-cxd4/package.mk
@@ -6,7 +6,7 @@ PKG_VERSION="d4adb3d0ad53337a9985fd97ed0afcad417f6ccc"
 PKG_SHA256="697d5acf02f44dc2a2551a84f429780512a1be0cf6b325c6e4f5dc0f445ea4ed"
 PKG_LICENSE="CC0-1.0"
 PKG_SITE="https://github.com/mupen64plus/mupen64plus-rsp-cxd4"
-PKG_URL="https://github.com/mupen64plus/mupen64plus-rsp-cxd4/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/mupen64plus/mupen64plus-rsp-cxd4/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc SDL2-system mupen64plus-core"
 PKG_LONGDESC="Exemplary MSP communications simulator using a normalized VU."
 PKG_TOOLCHAIN="make"
@@ -14,7 +14,7 @@ PKG_TOOLCHAIN="make"
 PKG_MAKE_OPTS_TARGET="-f projects/unix/Makefile SRCDIR=. APIDIR=$(get_build_dir mupen64plus-core)/src/api all HLEVIDEO=1"
 
 pre_configure_target() {
-  export SYSROOT_PREFIX=$SYSROOT_PREFIX
+  export SYSROOT_PREFIX=${SYSROOT_PREFIX}
 
   # ARCH arm
   if [ "${ARCH}" = "arm" ]; then

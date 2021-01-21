884880cf777b34e636586e39356bddbdf224bc30
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-21 20:06:37 +0100

    mupen64plus-ui-console: automated code cleanup

--- packages/supervisedthinking/emulation/standalone/mupen64plus/mupen64plus-depends/mupen64plus-ui-console/package.mk
+++ packages/supervisedthinking/emulation/standalone/mupen64plus/mupen64plus-depends/mupen64plus-ui-console/package.mk
@@ -6,7 +6,7 @@ PKG_VERSION="32e27344214946f0dce3cd2b4fff152a3538bd8f"
 PKG_SHA256="7d4a0a71545caec19d007f34038cffaee36b75d27de615cd4e175bb5ab2e227d"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/mupen64plus/mupen64plus-ui-console"
-PKG_URL="https://github.com/mupen64plus/mupen64plus-ui-console/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/mupen64plus/mupen64plus-ui-console/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc SDL2-system mupen64plus-core"
 PKG_LONGDESC="Console (command-line) front-end user interface for Mupen64Plus v2.0 project"
 PKG_TOOLCHAIN="make"
@@ -20,7 +20,7 @@ PKG_MAKE_OPTS_TARGET="-f projects/unix/Makefile \
                       all"
 
 pre_configure_target() {
-  export SYSROOT_PREFIX=$SYSROOT_PREFIX
+  export SYSROOT_PREFIX=${SYSROOT_PREFIX}
 }
 
 makeinstall_target() {

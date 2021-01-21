e17b0c6e5bd3e2ee3cfec26176e4803c0976c840
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-21 20:14:20 +0100

    common-overlays-lr: automated code cleanup

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/common-overlays-lr/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/common-overlays-lr/package.mk
@@ -6,12 +6,12 @@ PKG_VERSION="199019e120c321e667040c1a09ff9535e1547711"
 PKG_SHA256="7ad6cf6a0e1adae269c3ba2997231489240012614168e5fabf541b3679650d52"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/common-overlays"
-PKG_URL="https://github.com/libretro/common-overlays/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libretro/common-overlays/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET=""
 PKG_LONGDESC="Collection of overlay files for use with libretro frontends, such as RetroArch."
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/share/retroarch/overlay
-  cp -r $PKG_BUILD/borders $INSTALL/usr/share/retroarch/overlay
+  mkdir -p ${INSTALL}/usr/share/retroarch/overlay
+  cp -r ${PKG_BUILD}/borders ${INSTALL}/usr/share/retroarch/overlay
 }

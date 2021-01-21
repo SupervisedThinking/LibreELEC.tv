bebbbc90586109b3a719fdc40d5538b0fd881f93
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-21 20:12:05 +0100

    mupen64plus-audio-sdl2: automated code cleanup

--- packages/supervisedthinking/emulation/standalone/mupen64plus/mupen64plus-depends/mupen64plus-audio-sdl2/package.mk
+++ packages/supervisedthinking/emulation/standalone/mupen64plus/mupen64plus-depends/mupen64plus-audio-sdl2/package.mk
@@ -6,7 +6,7 @@ PKG_VERSION="4922bfa066c375a9b3f7957d38fb1ef04d9fe8f7"
 PKG_SHA256="78caacbaa7f7bcf8d6c96a2728a8d15bf0bbc54bf08beaf583647529b6db60eb"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/m64p/mupen64plus-audio-sdl2"
-PKG_URL="https://github.com/m64p/mupen64plus-audio-sdl2/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/m64p/mupen64plus-audio-sdl2/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc mupen64plus-core libsamplerate SDL2-system"
 PKG_LONGDESC="A low-level N64 video emulation plugin, based on the pixel-perfect angrylion RDP plugin with some improvements."
 PKG_TOOLCHAIN="make"
@@ -15,7 +15,7 @@ PKG_BUILD_FLAGS="+pic"
 PKG_MAKE_OPTS_TARGET="-f projects/unix/Makefile SRCDIR=src APIDIR=$(get_build_dir mupen64plus-core)/src/api all"
 
 pre_configure_target() {
-  export SYSROOT_PREFIX=$SYSROOT_PREFIX
+  export SYSROOT_PREFIX=${SYSROOT_PREFIX}
 }
 
 makeinstall_target() {

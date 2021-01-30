45a120b827a425395845461290e062722d99cace
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-31 00:46:30 +0100

    mesen: updated to git 70f1a1b / updated repo

--- packages/supervisedthinking/emulation/libretro/mesen/package.mk
+++ packages/supervisedthinking/emulation/libretro/mesen/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="mesen"
-PKG_VERSION="0.9.9"
-PKG_SHA256="8c638a9910b8de6cf628ceefb226c2f636e4397b0629f58a8ffb304984d68ea9"
+PKG_VERSION="70f1a1b600118fef2832f10f1dff67820a39490d" #0.9.9+
+PKG_SHA256="d24c13c48397041ad24555735a7a470c82a2b647166cb9e95c290e354e7ea48f"
 PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/SourMesen/Mesen"
-PKG_URL="https://github.com/SourMesen/Mesen/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/NovaSquirrel/Mesen"
+PKG_URL="https://github.com/NovaSquirrel/Mesen/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Mesen is a cross-platform (Windows & Linux) NES/Famicom emulator built in C++ and C#"
 PKG_TOOLCHAIN="make"

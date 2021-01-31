33c9a5cac3c6e02235c247bf6c41ab4a87c189f1
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-31 19:17:30 +0100

    sdl2_ttf: renamed / use lowercase deps

--- packages/supervisedthinking/multimedia/SDL/sdl2_ttf/package.mk
+++ packages/supervisedthinking/multimedia/SDL/sdl2_ttf/package.mk
@@ -1,13 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
-PKG_NAME="SDL2_ttf"
+PKG_NAME="sdl2_ttf"
 PKG_VERSION="2.0.15"
 PKG_SHA256="a9eceb1ad88c1f1545cd7bd28e7cbc0b2c14191d40238f531a15b01b1b22cd33"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.libsdl.org/"
 PKG_URL="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain SDL2-system freetype"
+PKG_DEPENDS_TARGET="toolchain sdl2 freetype"
 PKG_LONGDESC="This is a sample library which allows you to use TrueType fonts in your SDL applications"
 PKG_TOOLCHAIN="configure"
 

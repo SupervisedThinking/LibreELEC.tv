2f16a238c10395f99b9c6f1a4bef281610741e35
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-01 12:39:07 +0100

    xcb-util(s): add missing dependencies

--- packages/supervisedthinking/x11/util/xcb-util-keysyms/package.mk
+++ packages/supervisedthinking/x11/util/xcb-util-keysyms/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="0ef8490ff1dede52b7de533158547f8b454b241aa3e4dcca369507f66f216dd9"
 PKG_LICENSE="GPL"
 PKG_SITE="https://xcb.freedesktop.org/XcbUtil/"
 PKG_URL="https://xcb.freedesktop.org/dist/xcb-util-keysyms-${PKG_VERSION}.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain xcb-util"
+PKG_DEPENDS_TARGET="toolchain libxcb xorgproto util-macros"
 PKG_LONGDESC="The xcb-util-keysyms package contains a library for handling standard X key constants and conversion to/from keycodes."
 
 

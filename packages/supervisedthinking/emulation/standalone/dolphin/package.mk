1e6e96b65a2fbaffba1aa3370f643658c4acb135
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-03 16:00:47 +0100

    dolphin: updated to r5.0-13605

--- packages/supervisedthinking/emulation/standalone/dolphin/package.mk
+++ packages/supervisedthinking/emulation/standalone/dolphin/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="dolphin"
-PKG_VERSION="9d04fd1ccbb2973777cec14c4244f6a002e1da41" #5.0-13601
+PKG_VERSION="b6e9cca64f41d20d6eaca46f151e2efd07ccafbf" #5.0-13605
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/dolphin-emu/dolphin"

321eb94aa0c21d55a1208f33e9623f8ee1b7e060
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-08 16:36:27 +0100

    ppsspp: updated to v1.11

--- packages/supervisedthinking/emulation/standalone/ppsspp/package.mk
+++ packages/supervisedthinking/emulation/standalone/ppsspp/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="ppsspp"
-PKG_VERSION="19313aaabb922f4243fa46ce8e498456db9151b3" #v1.10.3
+PKG_VERSION="76a568a4d37539fc13f6f90da9f22ed62a9bdc6a" #v1.11
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/hrydgard/ppsspp"
 PKG_URL="https://github.com/hrydgard/ppsspp.git"

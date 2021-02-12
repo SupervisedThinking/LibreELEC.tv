44fa57eaae7ab330b0dfe3937b04d2ff01c5e572
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-12 16:07:51 +0100

    gtk: updated license

--- packages/supervisedthinking/toolkit/gtk/hicolor-icon-theme/package.mk
+++ packages/supervisedthinking/toolkit/gtk/hicolor-icon-theme/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="hicolor-icon-theme"
 PKG_VERSION="0.17"
 PKG_SHA256="317484352271d18cbbcfac3868eab798d67fff1b8402e740baa6ff41d588a9d8"
-PKG_LICENSE="GPL"
+PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://www.freedesktop.org/wiki/Software/icon-theme/"
 PKG_URL="https://icon-theme.freedesktop.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain gtk3-system"

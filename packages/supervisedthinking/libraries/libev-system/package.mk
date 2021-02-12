81927ce08dd5163d0f5004a49c75dc850e1953d1
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-12 15:27:06 +0100

    libev-system: updated license

--- packages/supervisedthinking/libraries/libev-system/package.mk
+++ packages/supervisedthinking/libraries/libev-system/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="libev-system"
 PKG_VERSION="4.33"
 PKG_SHA256="d6b41df38df2a7ced84e3dd14e3c34afd1c893f67af0086977973f51c4986214"
-PKG_LICENSE="GPL"
+PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="http://libev.schmorp.de"
 PKG_URL="https://git.lighttpd.net/mirrors/libev/archive/rel-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"

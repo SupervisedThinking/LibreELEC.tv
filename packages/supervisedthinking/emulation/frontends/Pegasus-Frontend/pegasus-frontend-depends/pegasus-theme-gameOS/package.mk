349d748a351c725885c982e20a9c397e11776b7a
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-12 15:17:55 +0100

    pegasus-the-gameOS: updated license

--- packages/supervisedthinking/emulation/frontends/Pegasus-Frontend/pegasus-frontend-depends/pegasus-theme-gameOS/package.mk
+++ packages/supervisedthinking/emulation/frontends/Pegasus-Frontend/pegasus-frontend-depends/pegasus-theme-gameOS/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="pegasus-theme-gameOS"
 PKG_VERSION="03812b4e721ef81ed2f5d6437e45c6fc63ad0f3f" # v1.10
 PKG_SHA256="391ab9137cdf816dddc9358b72828827b22eeb4cb8d9b57d518f0883712074a0"
-PKG_LICENSE="GPL"
+PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/PlayingKarrde/gameOS"
 PKG_URL="https://github.com/PlayingKarrde/gameOS/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"

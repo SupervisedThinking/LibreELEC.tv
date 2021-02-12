286bab7686daedb2c45ee5d8a1533e5809e2e488
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-12 16:07:43 +0100

    libcroco: updated license

--- packages/supervisedthinking/textproc/libcroco/package.mk
+++ packages/supervisedthinking/textproc/libcroco/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="libcroco"
 PKG_VERSION="0.6.12"
 PKG_SHA256="ddc4b5546c9fb4280a5017e2707fbd4839034ed1aba5b7d4372212f34f84f860"
-PKG_LICENSE="GPL"
+PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://www.freedesktop.org/wiki/Software/icon-theme/"
 PKG_URL="http://ftp.gnome.org/pub/gnome/sources/${PKG_NAME}/${PKG_VERSION:0:3}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain glib libxml2"

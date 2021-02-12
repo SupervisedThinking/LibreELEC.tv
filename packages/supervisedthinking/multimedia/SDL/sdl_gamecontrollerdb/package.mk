b47816ccf03f9c5be41b9f63015a44aa0464c807
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-12 14:36:51 +0100

    sdl_gamecontrollerdb: updated to git 9de9aef / reworked build opts
    now install the database to /usr/share/(...) and link to ~/.config/(...) to ensure updates

--- packages/supervisedthinking/multimedia/SDL/sdl_gamecontrollerdb/package.mk
+++ packages/supervisedthinking/multimedia/SDL/sdl_gamecontrollerdb/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="sdl_gamecontrollerdb"
-PKG_VERSION="49abd3b88102de231cc51f80fe791219bb40c2ba"
-PKG_SHA256="512fd4fb41093c7bf08feb9b330d18ab853a628828229f01f72224c6aa5d72e2"
-PKG_LICENSE="OSS"
+PKG_VERSION="9de9aef9f005e284d08a938ee1b512a0cc66bfe8"
+PKG_SHA256="2ebf5c29c4c72f4e41833addda084a722a20eac2719f86cc92da37fb786ab016"
+PKG_LICENSE="SDL"
 PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
 PKG_URL="https://github.com/gabomdq/SDL_GameControllerDB/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET=""
@@ -12,13 +12,21 @@ PKG_LONGDESC="A community sourced database of game controller mappings to be use
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  PKG_INSTALL_PATH=${INSTALL}/usr/config/SDL-GameControllerDB
-  mkdir -p ${PKG_INSTALL_PATH}
-  cp -v gamecontrollerdb.txt ${PKG_INSTALL_PATH}
-  # Clean up
-  sed -e "/Windows/d" -i ${PKG_INSTALL_PATH}/gamecontrollerdb.txt
-  sed -e "/Mac/d"     -i ${PKG_INSTALL_PATH}/gamecontrollerdb.txt
-  sed -e "/Android/d" -i ${PKG_INSTALL_PATH}/gamecontrollerdb.txt
-  sed -e "/iOS/d"     -i ${PKG_INSTALL_PATH}/gamecontrollerdb.txt
-  sed -e "/^$/d"      -i ${PKG_INSTALL_PATH}/gamecontrollerdb.txt
+  # Paths
+  PKG_CONFIG_PATH="/usr/config/SDL-GameControllerDB"
+  PKG_SHARE_PATH="/usr/share/sdl-gamecontrollerdb"
+  PKG_DATABASE_PATH="${PKG_BUILD}/gamecontrollerdb.txt"
+
+  # Clean up database
+  sed -e "/Windows/d" -i ${PKG_DATABASE_PATH}
+  sed -e "/Mac/d"     -i ${PKG_DATABASE_PATH}
+  sed -e "/Android/d" -i ${PKG_DATABASE_PATH}
+  sed -e "/iOS/d"     -i ${PKG_DATABASE_PATH}
+  sed -e "/^$/d"      -i ${PKG_DATABASE_PATH}
+
+  # Install files
+  mkdir -p ${INSTALL}/${PKG_CONFIG_PATH}
+  mkdir -p ${INSTALL}/${PKG_SHARE_PATH}
+   cp -v ${PKG_DATABASE_PATH} ${INSTALL}/${PKG_SHARE_PATH}
+   ln -sf ${PKG_SHARE_PATH}/gamecontrollerdb.txt ${INSTALL}/${PKG_CONFIG_PATH}/gamecontrollerdb.txt
 }

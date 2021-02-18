4476e2d02859229bf16162aca6e874dc3ea21b49
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-18 13:03:25 +0100

    virtualjaguar: initial package git afe223e / Jaguar

---
+++ packages/supervisedthinking/emulation/libretro/virtualjaguar/package.mk
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="virtualjaguar"
+PKG_VERSION="afe223ebae1dac64fe33874625d21ec51704274e"
+PKG_SHA256="b0f74f1d8ee13e16c06fe3aed449f1cf059f710cc2313f2fa82313e3a5f4db80"
+PKG_LICENSE="GPL-3.0-or-later"
+PKG_SITE="https://github.com/libretro/virtualjaguar-libretro"
+PKG_URL="https://github.com/libretro/virtualjaguar-libretro/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain glibc"
+PKG_LONGDESC="Port of Virtual Jaguar to Libretro"
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
+
+PKG_LIBNAME="virtualjaguar_libretro.so"
+PKG_LIBPATH="${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}

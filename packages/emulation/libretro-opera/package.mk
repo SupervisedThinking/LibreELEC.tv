c780d2bde38cdb4c83281ae61fb5cf7cd27c2d4c
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-18 16:29:21 +0100

    libretro-opera: downgrade to git 539dc44

--- packages/emulation/libretro-opera/package.mk
+++ packages/emulation/libretro-opera/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-opera"
-PKG_VERSION="5781f68e39e4af41aede43467964948af30a2547"
-PKG_SHA256="10fc173e6113396f8ce7aae419cee8ee71d0c6fe21da3e430ff72d4605951e8a"
+PKG_VERSION="539dc447b536ba52eb3485f11d9bf1c06e30cf75"
+PKG_SHA256="5552025528d8e9100b2f6729ab511f7708f36a687d2d07c379a0087ae7b81021"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/opera-libretro"
 PKG_URL="https://github.com/libretro/opera-libretro/archive/${PKG_VERSION}.tar.gz"

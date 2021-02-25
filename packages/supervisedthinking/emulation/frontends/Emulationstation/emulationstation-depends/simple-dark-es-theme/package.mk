540950ce00a7212f4168ee17c5e0fe2816361bb5
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-25 01:47:09 +0100

    simple-dark-es-theme: updated to git e4fd1d5 / switched repo / updated pkg build

--- packages/supervisedthinking/emulation/frontends/Emulationstation/emulationstation-depends/simple-dark-es-theme/package.mk
+++ packages/supervisedthinking/emulation/frontends/Emulationstation/emulationstation-depends/simple-dark-es-theme/package.mk
@@ -2,28 +2,21 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="simple-dark-es-theme"
-PKG_VERSION="058472cfbc3b4fe9ddf1ab452908fab40e32d29c"
-PKG_SHA256="8e58972134d54cf202109c240452c65f1672ece7cd7955a549b272bea3963094"
+PKG_VERSION="e4fd1d56dbcdd9b3273eaa5f252edd88718197be"
+PKG_SHA256="040b63bf178604e9ee0a4a9cbb0ff9950e5553edd037ef29699d2a8f9d45846d"
 PKG_LICENSE="CC-BY-NC-SA-2.0"
-PKG_SITE="https://github.com/RetroPie/es-theme-simple-dark"
-PKG_URL="https://github.com/RetroPie/es-theme-simple-dark/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/SupervisedThinking/es-theme-simple-dark"
+PKG_URL="https://github.com/SupervisedThinking/es-theme-simple-dark/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Simple dark theme for Emulationstation based on Theme 'simple' v1.3 - 11-29-2014"
-PKG_TOOLCHAIN="manual"
+PKG_TOOLCHAIN="make"
 
-makeinstall_target() {
+PKG_MAKE_OPTS_TARGET="install DESTDIR=${INSTALL}"
+
+post_makeinstall_target() {
   # Install stock theme
   ES_THEME_PATH=/usr/share/emulationstation/themes/simple-dark
-  mkdir -p ${INSTALL}/usr/config/emulationstation/themes
-  mkdir -p ${INSTALL}/${ES_THEME_PATH}
-  cp -r *  ${INSTALL}/${ES_THEME_PATH}
-  ln -s    ${ES_THEME_PATH} ${INSTALL}/usr/config/emulationstation/themes/simple-dark
-
-  # Add missing generic systems
-  if [ "${PROJECT}" = "Generic" ]; then
-    cp -a ${PKG_DIR}/files/n3ds ${INSTALL}/${ES_THEME_PATH}/
-  fi
-
-  # Add scroll sound to menu 
-  cp ${PKG_DIR}/files/scroll.wav ${INSTALL}/${ES_THEME_PATH}/art/
+  ES_CONFIG_PATH=/usr/config/emulationstation/themes
+  mkdir -p ${INSTALL}/${ES_CONFIG_PATH}
+    ln -s ${ES_THEME_PATH} ${INSTALL}/${ES_CONFIG_PATH}
 }

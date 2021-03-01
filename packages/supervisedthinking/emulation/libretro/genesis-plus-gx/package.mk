5b94eb24361d73531acdc0b9e9ce5e1e198d0747
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-01 14:29:28 +0100

    genesis-plus-gx: updated to git 0250731

--- packages/supervisedthinking/emulation/libretro/genesis-plus-gx/package.mk
+++ packages/supervisedthinking/emulation/libretro/genesis-plus-gx/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="genesis-plus-gx"
-PKG_VERSION="76a6ecb26d585718e76e7f9dbef88f85a2ae51c4"
-PKG_SHA256="d98d9e141043737499dbf9f2f9c72e2dda35c0eeb8804d1d89d94a7dedc699a1"
+PKG_VERSION="02507311c4059ca11b2cb74c87f7d92e8a0f7b88"
+PKG_SHA256="27be82d32f7b58cc81ce67231f07a80dcca80b2ee3ca3ec61472849dc53295dd"
 PKG_LICENSE="Modified BSD / LGPLv2.1"
 PKG_SITE="https://github.com/libretro/Genesis-Plus-GX"
 PKG_URL="https://github.com/libretro/Genesis-Plus-GX/archive/${PKG_VERSION}.tar.gz"

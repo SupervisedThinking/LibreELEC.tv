aadef7c3df8b356de227250e8e72a840831a337a
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-01 11:13:51 +0100

    stella2014: updated to git 98e197e / added platform armv make opts / updated license

--- packages/supervisedthinking/emulation/libretro/stella2014/package.mk
+++ packages/supervisedthinking/emulation/libretro/stella2014/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="stella2014"
-PKG_VERSION="62e9468239576a3f3bc64845066aed2dea6ea6b1"
-PKG_SHA256="cd5716fd447ed80e12f374d063c771a16f2b8e07af469a84c73ca770925698b8"
-PKG_LICENSE="GPLv2"
+PKG_VERSION="98e197e745d0ec52fc2d5b234ed15881267dd19f"
+PKG_SHA256="5cd2a4663b9b7ed9d49ff19677145f797b78ba129896ac57a58e5c8f529fd06c"
+PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/stella2014-libretro"
 PKG_URL="https://github.com/libretro/stella2014-libretro/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
@@ -16,6 +16,17 @@ PKG_LIBNAME="stella2014_libretro.so"
 PKG_LIBPATH="${PKG_LIBNAME}"
 
 PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_configure_target() {
+  if [ "${ARCH}" = "arm" ]; then
+    PKG_MAKE_OPTS_TARGET+=" platform=armv"
+
+    # NEON Support ?
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+="-neon"
+    fi
+  fi
+}
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro

70dca506d02475cef62f0cf91cc7b1bd4098e9da
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-02 20:02:19 +0100

    fbneo: updated to git 103dd78

--- packages/supervisedthinking/emulation/libretro/fbneo/package.mk
+++ packages/supervisedthinking/emulation/libretro/fbneo/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="fbneo"
-PKG_VERSION="899a9f752d3270258b452f993e3d2f3a989e263a"
-PKG_SHA256="78ffebb796dd0aa8d1821bd0fa2ae2eb27919fe6eb6a8b02f5eafd4e8a36b6ce"
+PKG_VERSION="103dd78218d23af1b65a610a60e67928aceedc53"
+PKG_SHA256="b36c9eb25c41ec6e82cb4fab0e76f249071a714b0c86627b83a3d27e74614c0f"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/libretro/FBNeo"
 PKG_URL="https://github.com/libretro/FBNeo/archive/${PKG_VERSION}.tar.gz"

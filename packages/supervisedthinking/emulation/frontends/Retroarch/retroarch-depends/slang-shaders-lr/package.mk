b5b7c16a26da46d3fa9a18dca65254e5c17141f0
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-02 12:26:25 +0100

    slang-shaders-lr: updated to git 12fdb65

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/slang-shaders-lr/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/slang-shaders-lr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="slang-shaders-lr"
-PKG_VERSION="35c2d7064e7a7a9bec76048ed753ea74fe8c1ba0"
-PKG_SHA256="eed4f2d7e82bc8c298e751ccae9310caeff85e569a9fe2e8051a413fa47495b6"
+PKG_VERSION="12fdb6562ff0b796bc4dc48fddce576b4eea6db9"
+PKG_SHA256="66095e156df726cd4074277082742af2c50c8dcfb67121db14f51c26d8e6c210"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/slang-shaders"
 PKG_URL="https://github.com/libretro/slang-shaders/archive/${PKG_VERSION}.tar.gz"

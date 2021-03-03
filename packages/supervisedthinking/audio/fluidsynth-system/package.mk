5c3a9a8eb5640dcfc0994fb1bd860e671c96ccc5
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-03 19:32:46 +0100

    fluidsynth-system: added soundfont-fluidr3mono_gm_sf3 dependency / clean up

--- packages/supervisedthinking/audio/fluidsynth-system/package.mk
+++ packages/supervisedthinking/audio/fluidsynth-system/package.mk
@@ -7,29 +7,27 @@ PKG_SHA256="365a1c0982efcaff724a7b05d26ce1db76bc7435aa4c239df61cbc87f04b6c90"
 PKG_LICENSE="LGPL-2.1-or-later"
 PKG_SITE="http://fluidsynth.org/"
 PKG_URL="https://github.com/FluidSynth/fluidsynth/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain glib soundfont-generaluser libsndfile-system"
-PKG_LONGDESC="FluidSynth is a software real-time synthesizer based on the Soundfont 2 specifications."
+PKG_DEPENDS_TARGET="toolchain glib soundfont-generaluser soundfont-fluidr3mono_gm_sf3 libsndfile-system"
+PKG_LONGDESC="FluidSynth is a software real-time synthesizer based on the Soundfont specifications."
 PKG_BUILD_FLAGS="+pic"
 PKG_DEPENDS_CONFIG="libsndfile-system"
 
 pre_configure_target() {
-  PKG_CMAKE_OPTS_TARGET="-DLIB_SUFFIX= \
-                         -Denable-readline=0 \
-                         -Denable-oss=0 \
-                         -Denable-pulseaudio=1 \
-                         -Denable-libsndfile=1"
+  PKG_CMAKE_OPTS_TARGET="-D LIB_SUFFIX= \
+                         -D enable-readline=0 \
+                         -D enable-oss=0 \
+                         -D enable-pulseaudio=1 \
+                         -D enable-libsndfile=1"
 }
 
 post_makeinstall_target() {
   # Create directories
   mkdir -p ${INSTALL}/etc/fluidsynth
-  mkdir -p ${INSTALL}/usr/config/fluidsynth/soundfonts
+  mkdir -p ${INSTALL}/usr/config/fluidsynth
+  mkdir -p ${INSTALL}/usr/config/soundfonts
 
   # Create symlinks & install config file
   cp -a ${PKG_DIR}/config/* ${INSTALL}/usr/config/fluidsynth/
-  ln -s /storage/.config/fluidsynth/fluidsynth.conf ${INSTALL}/etc/fluidsynth/
-  echo "Place your SoundFonts here!" >>             ${INSTALL}/usr/config/fluidsynth/soundfonts/readme.txt
-
-  # Create symlink to SoundFont
-  ln -s /usr/share/soundfonts/GeneralUser.sf2  ${INSTALL}/usr/config/fluidsynth/soundfonts/
+  ln -sf /storage/.config/fluidsynth/fluidsynth.conf ${INSTALL}/etc/fluidsynth/
+  echo "Place your SoundFonts here!"              >> ${INSTALL}/usr/config/soundfonts/readme.txt
 }

c763c3c0fa9460e76987015baeb9ef833e6d41e8
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-09 15:25:12 +0100

    libsndfile-system: add opus-system dependency

--- packages/supervisedthinking/audio/libsndfile-system/package.mk
+++ packages/supervisedthinking/audio/libsndfile-system/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="8cdee0acb06bb0a3c1a6ca524575643df8b1f3a55a0893b4dd9f829d08263785"
 PKG_LICENSE="LGPL-2.1-or-later"
 PKG_SITE="https://libsndfile.github.io/libsndfile/"
 PKG_URL="https://github.com/libsndfile/libsndfile/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib flac-system libvorbis-system"
+PKG_DEPENDS_TARGET="toolchain alsa-lib flac-system libvorbis-system opus-system"
 PKG_LONGDESC="A C library for reading and writing sound files containing sampled audio data."
 PKG_BUILD_FLAGS="+pic -sysroot"
 

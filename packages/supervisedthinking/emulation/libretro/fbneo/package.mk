b3bc83c6f0af9de785166893c944bd28a9c2c379
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-11 18:12:18 +0100

    fbneo: updated to git 5d6704a / enabled lto & disabled gold

--- packages/supervisedthinking/emulation/libretro/fbneo/package.mk
+++ packages/supervisedthinking/emulation/libretro/fbneo/package.mk
@@ -2,14 +2,15 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="fbneo"
-PKG_VERSION="295d5ab66a7ae879ce45d5f9ea25e84c5ed922e9"
-PKG_SHA256="ae37dd37b6339e1a3faa6005f8d785dc7f5e1381b9bf6f61e19396ed57911815"
+PKG_VERSION="5d6704a4fdb3b91b53426ce587b9f8a255129e50"
+PKG_SHA256="e5ba193ab59d4e8380b952a50905bab0efb3f9167e00858f95132ba410068c0d"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/libretro/FBNeo"
 PKG_URL="https://github.com/libretro/FBNeo/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="A libretro port of FinalBurn Neo for Romset v0.2.97.44"
 PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="-gold +lto"
 
 PKG_LIBNAME="fbneo_libretro.so"
 PKG_LIBPATH="src/burner/libretro/${PKG_LIBNAME}"

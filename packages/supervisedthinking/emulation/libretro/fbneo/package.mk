3fa3f28f669c160f6f0a00f5df26d73c3f90a313
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-12 01:05:04 +0100

    fbneo: updated to git ac99b2a / updated Romset version

--- packages/supervisedthinking/emulation/libretro/fbneo/package.mk
+++ packages/supervisedthinking/emulation/libretro/fbneo/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="fbneo"
-PKG_VERSION="5d6704a4fdb3b91b53426ce587b9f8a255129e50"
-PKG_SHA256="e5ba193ab59d4e8380b952a50905bab0efb3f9167e00858f95132ba410068c0d"
+PKG_VERSION="ac99b2ab1e7793b255f1bb410804a80deed473b0"
+PKG_SHA256="774b5b9935fc7f8b1b118fee1062eb19d1f1a9780b8e8af160407735a82f6d66"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/libretro/FBNeo"
 PKG_URL="https://github.com/libretro/FBNeo/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
-PKG_LONGDESC="A libretro port of FinalBurn Neo for Romset v0.2.97.44"
+PKG_LONGDESC="A libretro port of FinalBurn Neo for Romset v1.0.0.1"
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="-gold +lto"
 

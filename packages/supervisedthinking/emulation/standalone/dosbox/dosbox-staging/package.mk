8e892633a3b306db625846cd807771be7039ea4d
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-15 11:28:21 +0100

    dosbox-staging: updated to git 49148e6 / added meson config summary patch

--- packages/supervisedthinking/emulation/standalone/dosbox/dosbox-staging/package.mk
+++ packages/supervisedthinking/emulation/standalone/dosbox/dosbox-staging/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="dosbox-staging"
-PKG_VERSION="410042775b29cb8ffafdc187e8086c4274c8b002" #v0.77.0-dev
+PKG_VERSION="49148e600bbd9725471aedc809b003636c9f688d" #v0.77.0-dev
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://dosbox-staging.github.io/"
 PKG_URL="https://github.com/dosbox-staging/dosbox-staging.git"
